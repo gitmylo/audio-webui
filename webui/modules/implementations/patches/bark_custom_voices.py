a082de950b137a509a2f0de3ac9b866b66cad3b0
Mylo <36931363+gitmylo@users.noreply.github.com>
2023-05-09 18:02:23 +0200

    Implemented speech conversion

--- webui/modules/implementations/patches/bark_custom_voices.py
+++ webui/modules/implementations/patches/bark_custom_voices.py
@@ -1,13 +1,15 @@
 import tempfile
+from pathlib import Path
 
 import numpy
 import torch
 import torchaudio
+from TTS.utils.manage import ModelManager
+from TTS.utils.synthesizer import Synthesizer
 from bark.generation import SAMPLE_RATE, load_codec_model
 from scipy.io.wavfile import write as write_wav
 
 from scripts.bark_speaker_info import codec_decode
-from webui.modules.implementations.patches.bark_api import semantic_to_waveform_new
 from webui.modules.implementations.patches.bark_generation import generate_text_semantic_new, generate_coarse_new, generate_fine_new
 from encodec.utils import convert_audio
 from TTS.api import TTS
@@ -26,10 +28,53 @@ def patch_speaker_npz(voice_to_clone: str, npz_file: str):
     output.name += '.wav'
 
     # Convert speaker
-    tts = TTS(model_name="tts_models/multilingual/multi-dataset/your_tts", progress_bar=True, gpu=args.tts_use_cpu,
-              force_voice_convert=True)  # Monkeypatched flag to allow voice convert on your_tts
-    tts.manager.output_prefix = 'data/models/coqui-tts/'  # TODO: force downloading into this dir
-    tts.voice_conversion_to_file(source_wav=voice_to_clone, target_wav=fine_file.name, file_path=output.name)
+    model_name = 'tts_models/multilingual/multi-dataset/your_tts'
+    language_idx = 'en'
+    gpu = not args.tts_use_cpu
+
+    speaker_wav = voice_to_clone
+    reference_wav = fine_file.name
+
+    speakers_file_path = None
+    language_ids_file_path = None
+    encoder_path = None
+    encoder_config_path = None
+    vocoder_path = None
+    vocoder_config_path = None
+
+    import TTS.api
+    path = Path(TTS.api.__file__).parent / ".models.json"
+    manager = ModelManager(path, progress_bar=True)
+    model_path, config_path, model_item = manager.download_model(model_name)
+
+    tts_path = model_path
+    tts_config_path = config_path
+    if "default_vocoder" in model_item:
+        vocoder_name = model_item["default_vocoder"]
+
+    if vocoder_name is not None and not args.vocoder_path:
+        vocoder_path, vocoder_config_path, _ = manager.download_model(args.vocoder_name)
+
+    synthesizer = Synthesizer(
+        tts_checkpoint=tts_path,
+        tts_config_path=tts_config_path,
+        tts_speakers_file=speakers_file_path,
+        tts_languages_file=language_ids_file_path,
+        vocoder_checkpoint=vocoder_path,
+        vocoder_config=vocoder_config_path,
+        vc_checkpoint=encoder_path,
+        vc_config=encoder_config_path,
+        use_cuda=gpu
+    )
+
+    wav = synthesizer.tts(  # Why does it use TTS for voice conversion on vits?
+        language_name=language_idx,
+        speaker_wav=speaker_wav,
+        reference_wav=reference_wav,
+
+    )
+
+    synthesizer.save_wav(wav, output.name)
 
     # Convert the new fine and new coarse prompts
     new_fine, _ = generate_fine_from_wav(output.name)
