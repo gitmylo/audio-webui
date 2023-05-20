49ac3a4be5dc1b2f27bca42dfda253ba8ab74715
Mylo <36931363+gitmylo@users.noreply.github.com>
2023-05-20 16:47:15 +0200

    Fix for python install with spaces on some systems

--- setup_tools/venv.py
+++ setup_tools/venv.py
@@ -22,7 +22,7 @@ def activate_venv():
         return
     if not os.path.isdir(venv_name):
         print('no venv found, creating venv')
-        run_command(get_python(), '-m venv venv')
+        run_command(f'"{get_python()}"', '-m venv venv')
     run_command([('call' if is_windows() else 'source', venv_activate_path), ('python', ' '.join([f'"{arg}"' for arg in sys.argv]))])  # Launch the main.py with the venv
     exit()  # Exit after the venv'ed version exits (maximum depth will be 2 because the venv is already activated in that case)
 
