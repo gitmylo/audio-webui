9d8e0dd9ad244e747935358096b94216939843c5
Mylo <36931363+gitmylo@users.noreply.github.com>
2023-05-21 22:41:57 +0200

    Make model downloads from manager more flexible, custom hubert models and load tokenizer models from other repositories.

--- hubert/hubert_manager.py
+++ hubert/hubert_manager.py
@@ -7,30 +7,27 @@ import huggingface_hub
 
 class HuBERTManager:
     @staticmethod
-    def make_sure_hubert_installed():
+    def make_sure_hubert_installed(download_url: str = 'https://dl.fbaipublicfiles.com/hubert/hubert_base_ls960.pt', file_name: str = 'hubert.pt'):
         install_dir = os.path.join('data', 'models', 'hubert')
         if not os.path.isdir(install_dir):
             os.makedirs(install_dir, exist_ok=True)
-        install_file = os.path.join(install_dir, 'hubert.pt')
-        download_url = 'https://dl.fbaipublicfiles.com/hubert/hubert_base_ls960.pt'
+        install_file = os.path.join(install_dir, file_name)
         if not os.path.isfile(install_file):
-            print('Downloading HuBERT small model')
+            print('Downloading HuBERT base model')
             urllib.request.urlretrieve(download_url, install_file)
             print('Downloaded HuBERT')
         return install_file
 
 
     @staticmethod
-    def make_sure_tokenizer_installed():
+    def make_sure_tokenizer_installed(model: str = 'quantifier_hubert_base_ls960_14.pth', repo: str = 'GitMylo/bark-voice-cloning', local_file: str = 'tokenizer.pth'):
         install_dir = os.path.join('data', 'models', 'hubert')
         if not os.path.isdir(install_dir):
             os.makedirs(install_dir, exist_ok=True)
-        install_file = os.path.join(install_dir, 'tokenizer.pth')
-        repo = 'GitMylo/bark-voice-cloning'
-        file = 'quantifier_hubert_base_ls960_14.pth'
+        install_file = os.path.join(install_dir, local_file)
         if not os.path.isfile(install_file):
             print('Downloading HuBERT custom tokenizer')
-            huggingface_hub.hf_hub_download(repo, file, local_dir=install_dir, local_dir_use_symlinks=False)
-            shutil.move(os.path.join(install_dir, file), install_file)
+            huggingface_hub.hf_hub_download(repo, model, local_dir=install_dir, local_dir_use_symlinks=False)
+            shutil.move(os.path.join(install_dir, model), install_file)
             print('Downloaded tokenizer')
         return install_file
