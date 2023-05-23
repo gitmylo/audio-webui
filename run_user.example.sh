93b9aa8ce22f81528865710d80ad4a8b7ff2c430
Mylo <36931363+gitmylo@users.noreply.github.com>
2023-05-23 18:56:55 +0200

    sh compatibility

--- run_user.example.sh
+++ run_user.example.sh
@@ -1,3 +1,3 @@
 # Run the webui with skip install and bark cpu offloading
 echo Starting webui, not installing packages. If you want to install packages. Run run.bat or install.bat instead.
-source run.sh -si --bark-cpu-offload "$@"
+source ./run.sh -si --bark-cpu-offload "$@"
