9c80d10564dcc37de40ceb3ab2c2880a72d6407e
Mylo <36931363+gitmylo@users.noreply.github.com>
2023-06-05 16:22:28 +0200

    Exceptions in case something unexpected goes wrong

--- webui/modules/implementations/audioldm.py
+++ webui/modules/implementations/audioldm.py
@@ -12,16 +12,22 @@ def create_model(pretrained='cvssp/audioldm-m-full', device='cuda' if torch.cuda
     if is_loaded():
         delete_model()
     global model, loaded
-    model = diffusers.AudioLDMPipeline.from_pretrained(pretrained, cache_dir=os.path.join('data', 'models', 'audioldm')).to(device)
-    loaded = True
+    try:
+        model = diffusers.AudioLDMPipeline.from_pretrained(pretrained, cache_dir=os.path.join('data', 'models', 'audioldm')).to(device)
+        loaded = True
+    except:
+        pass
 
 
 def delete_model():
     global model, loaded
-    del model
-    gc.collect()
-    torch.cuda.empty_cache()
-    loaded = False
+    try:
+        del model
+        gc.collect()
+        torch.cuda.empty_cache()
+        loaded = False
+    except:
+        pass
 
 
 def is_loaded():
@@ -30,6 +36,9 @@ def is_loaded():
 
 def generate(prompt='', negative_prompt='', steps=10, duration=5.0, cfg=2.5):
     if is_loaded():
-        return 16000, model(prompt if prompt else None, negative_prompt=negative_prompt if negative_prompt else None,
-                            audio_length_in_s=duration, num_inference_steps=steps, guidance_scale=cfg).audios[0]
+        try:
+            return 16000, model(prompt if prompt else None, negative_prompt=negative_prompt if negative_prompt else None,
+                                audio_length_in_s=duration, num_inference_steps=steps, guidance_scale=cfg).audios[0]
+        except Exception as e:
+            return f'An exception occurred: {str(e)}'
     return 'No model loaded! Please load a model first.'
