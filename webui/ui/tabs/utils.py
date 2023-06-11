4b60a1347c8cb735e7a35f1156d9ca16bebf3eb4
Mylo <36931363+gitmylo@users.noreply.github.com>
2023-06-11 17:21:11 +0200

    Update utils for batch audio separation #13

--- webui/ui/tabs/utils.py
+++ webui/ui/tabs/utils.py
@@ -1,5 +1,12 @@
+import os.path
+import time
+
 import gradio
+import numpy
 import torch
+import torchaudio
+import torchaudio.functional as F
+
 import webui.ui.tabs.rvc as rvc
 
 
@@ -12,10 +19,41 @@ def denoise_tab():
     def denoise_func(audio):
         sr, wav = audio
         import noisereduce.noisereduce as noisereduce
-        wav = noisereduce.reduce_noise(y=wav, sr=sr)
+        wav = noisereduce.reduce_noise(wav, sr)
         return sr, wav
 
+    with gradio.Row():
+        with gradio.Column():
+            in_directory = gradio.Textbox(label='Input directory')
+            out_directory = gradio.Textbox(label='Output directory')
+        batch_progress = gradio.Textbox(label='Batch processing progress')
+    denoise_batch = gradio.Button('Denoise batch', variant='primary')
+
+    def batch_denoise(in_dir, out_dir):
+        import noisereduce.noisereduce as noisereduce
+        if not os.path.isdir(in_dir):
+            yield 'Error: input directory is not a directory'
+            return
+        os.makedirs(out_dir, exist_ok=True)
+        output = f'Processing directory {in_dir}'
+        yield output
+        for f in os.listdir(in_dir):
+            if f.split('.')[-1] not in ['wav', 'mp3']:
+                continue
+            output += f'\nProcessing {f}'
+            yield output
+            full_path = os.path.join(in_dir, f)
+            wav, sr = torchaudio.load(full_path)
+            wav = wav.detach().cpu().numpy()
+            wav = noisereduce.reduce_noise(wav, sr)
+            wav = torch.tensor(wav)
+            torchaudio.save(os.path.join(out_dir, f), wav, sr)
+        output += '\nCompleted!'
+        yield output
+
     denoise_button.click(fn=denoise_func, inputs=audio_in, outputs=audio_out)
+    denoise_batch.click(fn=batch_denoise, inputs=[in_directory, out_directory], outputs=batch_progress)
+
 
 
 def music_split_tab():
@@ -43,6 +81,53 @@ def music_split_tab():
 
     with gradio.Row():
         with gradio.Column():
+            in_directory = gradio.Textbox(label='Input directory')
+            out_directory = gradio.Textbox(label='Output directory')
+        batch_progress = gradio.Textbox(label='Batch processing progress')
+    split_batch = gradio.Button('Denoise batch', variant='primary')
+
+    def batch_music_split(in_dir, out_dir):
+        if not os.path.isdir(in_dir):
+            yield 'Error: input directory is not a directory'
+            return
+        os.makedirs(os.path.join(out_dir, 'vocal'), exist_ok=True)
+        os.makedirs(os.path.join(out_dir, 'background'), exist_ok=True)
+        output = f'Processing directory {in_dir}'
+        yield output
+        for f in os.listdir(in_dir):
+            split = f.split('.')
+            extension = split[-1]
+            if extension not in ['wav', 'mp3']:
+                continue
+            output += f'\nProcessing {f}'
+            yield output
+            full_path = os.path.join(in_dir, f)
+            wav, sr = torchaudio.load(full_path)
+            # Split
+            if wav.dtype == numpy.int16:
+                wav = wav.float() / 32767.0
+            if wav.shape[0] == 2:
+                wav = wav.mean(0)
+            import webui.modules.implementations.rvc.split_audio as split_audio
+            vocal, background, sr = split_audio.split(sr, wav)
+            if vocal.shape[0] == 2:
+                vocal = vocal.mean(0)
+            if background.shape[0] == 2:
+                background = background.mean(0)
+            if len(vocal.shape) == 1:
+                vocal = vocal.unsqueeze(0)
+            if len(background.shape) == 1:
+                background = background.unsqueeze(0)
+
+            torchaudio.save(os.path.join(out_dir, 'vocal', f), vocal, sr)
+            torchaudio.save(os.path.join(out_dir, 'background', f), background, sr)
+        output += '\nCompleted!'
+        yield output
+
+    split_batch.click(fn=batch_music_split, inputs=[in_directory, out_directory], outputs=batch_progress)
+
+    with gradio.Row():
+        with gradio.Column():
             # audio_combine_1 = gradio.Audio(label='Input audio 1', type='filepath')
             audio_combine_1 = gradio.File(label='Input audio 1')
             # audio_combine_2 = gradio.Audio(label='Input audio 2', type='filepath')
@@ -50,7 +135,6 @@ def music_split_tab():
         audio_out = gradio.Audio(label='Combined audio')
 
     def music_merge_func(audio1, audio2):
-        import torchaudio
         x, sr = torchaudio.load(audio1.name)
         y, sry = torchaudio.load(audio2.name)
 
@@ -66,7 +150,6 @@ def music_split_tab():
         len_x = x.shape[-1] / sr
         len_y = y.shape[-1] / sry
 
-        import torchaudio.functional as F
         y = F.resample(y, sry, sr)
         y = F.resample(y, sr, int(sr * len_x/len_y))
         y = y.flatten()
