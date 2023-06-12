d636a4aed3bc1d6eef97833f1fb13f615027a440
Mylo <36931363+gitmylo@users.noreply.github.com>
2023-06-12 14:03:31 +0200

    Audio feature extraction #11

--- webui/ui/tabs/training/training/rvc_workspace.py
+++ webui/ui/tabs/training/training/rvc_workspace.py
@@ -1,16 +1,24 @@
+import gc
 import os
 import shutil
 
 import huggingface_hub
+import soundfile
+import torch.cuda
+import torch.nn.functional as F
+from fairseq import checkpoint_utils
 from scipy import signal
 
 import librosa
 import numpy as np
 from scipy.io import wavfile
 
+from hubert import hubert_manager
+from webui.args import args
 from webui.modules.implementations.rvc.slicer2 import Slicer
-from webui.ui.tabs.training.training.workspace import Workspace
+from webui.modules.implementations.rvc.custom_pitch_extraction import pitch_extract as pe
 from webui.modules.implementations.rvc.rvc import load_audio
+from webui.ui.tabs.training.training.workspace import Workspace
 
 workspace_path = os.path.join('data', 'training', 'RVC')
 
@@ -58,6 +66,20 @@ def norm_write(tmp_audio, gt_wavs_dir, wavs16k_dir, name, sr, max, alpha):
     )
 
 
+def readwave(wav_path, normalize=False):
+    wav, sr = soundfile.read(wav_path)
+    assert sr == 16000
+    feats = torch.from_numpy(wav).float()
+    if feats.dim() == 2:  # double channels
+        feats = feats.mean(-1)
+    assert feats.dim() == 1, feats.dim()
+    if normalize:
+        with torch.no_grad():
+            feats = F.layer_norm(feats, feats.shape)
+    feats = feats.view(1, -1)
+    return feats
+
+
 def process_dataset():
     if current_workspace is None:
         yield 'No workspace loaded, please load a workspace first.'
@@ -67,8 +89,8 @@ def process_dataset():
     dir_gt = os.path.join(current_workspace.space_path, '0_gt')
     dir_16k = os.path.join(current_workspace.space_path, '0_16k')
 
-    shutil.rmtree(dir_gt)
-    shutil.rmtree(dir_16k)
+    shutil.rmtree(dir_gt, ignore_errors=True)
+    shutil.rmtree(dir_16k, ignore_errors=True)
 
     os.makedirs(dir_gt, exist_ok=True)
     os.makedirs(dir_16k, exist_ok=True)
@@ -122,7 +144,7 @@ def process_dataset():
                 norm_write(tmp_audio, dir_gt, dir_16k, str(i), sr, max, alpha)
 
         except Exception as e:
-            output += f'\nException {e}. Skipping'
+            output += f'\nException {e} Skipping'
             yield output
 
 
@@ -130,8 +152,137 @@ def process_dataset():
     yield output
 
 
+def coarse_f0(f0, f0_bin, f0_mel_min, f0_mel_max):
+    f0_mel = 1127 * np.log(1 + f0 / 700)
+    f0_mel[f0_mel > 0] = (f0_mel[f0_mel > 0] - f0_mel_min) * (
+        f0_bin - 2
+    ) / (f0_mel_max - f0_mel_min) + 1
+
+    # use 0 or 1
+    f0_mel[f0_mel <= 1] = 1
+    f0_mel[f0_mel > f0_bin - 1] = f0_bin - 1
+    f0_coarse = np.rint(f0_mel).astype(np.int)
+    assert f0_coarse.max() <= 255 and f0_coarse.min() >= 1, (
+        f0_coarse.max(),
+        f0_coarse.min(),
+    )
+    return f0_coarse
+
+
 def pitch_extract():
-    pass
+    data = current_workspace.data
+    space_path = current_workspace.space_path
+    f0_method = data['f0']
+
+    sr = 16_000
+    hop = 160
+    f0_bin = 256
+    f0_max = 1100.0
+    f0_min = 50.0
+    f0_mel_min = 1127 * np.log(1 + f0_min / 700)
+    f0_mel_max = 1127 * np.log(1 + f0_max / 700)
+    input_dir = os.path.join(space_path, '0_16k')  # %s/1_16k_wavs
+    output_f0 = os.path.join(space_path, '1_f0')
+    output_f0nsf = os.path.join(space_path, '1_f0nsf')
+    output_feat = os.path.join(space_path, '1_feat')  # %s/3_feature256
+    time_step = 160 / 16000 * 1000
+
+    shutil.rmtree(output_f0, ignore_errors=True)
+    shutil.rmtree(output_f0nsf, ignore_errors=True)
+    shutil.rmtree(output_feat, ignore_errors=True)
+
+
+    os.makedirs(output_f0, exist_ok=True)
+    os.makedirs(output_f0nsf, exist_ok=True)
+    os.makedirs(output_feat, exist_ok=True)
+
+    output = 'Processing pitch...'
+    yield output
+
+    if f0_method != 'none':
+        for f in os.listdir(input_dir):
+            try:
+                output += f'\nExtracting pitch from {f}'
+                full_path = os.path.join(input_dir, f)
+                npy_name = os.path.splitext(f)[0] + '.npy'
+                npy_path_f0 = os.path.join(output_f0, npy_name)
+                npy_path_f0nsf = os.path.join(output_f0nsf, npy_name)
+                x = load_audio(full_path, sr)
+                p_len = x.shape[0] // hop
+                f0 = pe(f0_method, x, f0_min, f0_max, p_len, time_step, 16000, hop, 128)
+                np.save(npy_path_f0nsf, f0)
+                coarse_pitch = coarse_f0(f0, f0_bin, f0_mel_min, f0_mel_max)
+                np.save(npy_path_f0, coarse_pitch)
+            except Exception as e:
+                output += f'\nException: {e} Skipping'
+                yield output
+
+    output += '\nLoading HuBERT model...'
+    yield output
+
+    models, saved_cfg, task = checkpoint_utils.load_model_ensemble_and_task(
+        [hubert_manager.HuBERTManager.make_sure_hubert_rvc_installed()],
+        suffix="",
+    )
+
+    device = 'cuda' if (torch.cuda.is_available()) else 'cpu'
+
+    model = models[0]
+    model = model.to(device)
+    if device != "cpu":
+        model = model.half()
+    model.eval()
+
+    for idx, file in enumerate(os.listdir(input_dir)):
+        try:
+            output += f'\nProcessing {file}'
+            yield output
+
+            in_path = os.path.join(input_dir, file)
+            out_path = os.path.join(output_feat, os.path.splitext(file)[0] + '.npy')
+
+            if os.path.exists(out_path):
+                continue
+
+            features = readwave(in_path, normalize=saved_cfg.task.normalize)
+
+            padding_mask = torch.BoolTensor(features.shape).fill_(False)
+            inputs = {
+                "source": features.half().to(device)
+                if device not in ["mps", "cpu"]
+                else features.to(device),
+                "padding_mask": padding_mask.to(device),
+                "output_layer": 9 if features == "v1" else 12,  # layer 9
+            }
+
+            with torch.no_grad():
+                logits = model.extract_features(**inputs)
+                feats = (
+                    model.final_proj(logits[0]) if data['v'] == 1 else logits[0]
+                )
+
+            feats = feats.squeeze(0).float().cpu().numpy()
+            if np.isnan(feats).sum() == 0:
+                np.save(out_path, feats, allow_pickle=False)
+            else:
+                output += f'\n{file} contains NaN'
+                yield output
+
+        except Exception as e:
+            output += f'\nException: {e}'
+            yield output
+
+    output += '\nProcessing features...'
+    yield output
+
+
+    del model
+    del models
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    output += '\nDone!'
+    yield output
 
 
 def download_base_models(version_sr):
