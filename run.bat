7d9806e866972b5f0557ed5da9571a7ac7842cd0
Mylo <36931363+gitmylo@users.noreply.github.com>
2023-06-25 12:21:46 +0200

    Attempt python3.10 on sh scripts.

--- run.bat
+++ run.bat
@@ -3,5 +3,7 @@ where py -V > nul 2> nul
 if %errorlevel%==0 (
     call run_py.bat %*
 ) else (
+    echo WARNING: Unable to run py command, you probably do not have py launcher installed, make sure you have the python.org version installed, not the windows store version.
+    echo Launching anyways, running python command.
     call run_python.bat %*
 )
