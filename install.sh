acea0f6f72de8b9e9f95c2707287f103809a0cf8
Mylo <36931363+gitmylo@users.noreply.github.com>
2023-06-26 21:54:21 +0200

    Update install.sh to attempt to use python3.10

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
-python install.py "$@"
+if ! type "python3.10" > /dev/null; then
+  python3.10 install.py "$@"
+else
+  echo 'WARNING: python3.10 command was not found, attempting with python command, this could fail.'
+  python install.py "$@"
+fi
+
 read -n1 -r -p "Press any key to exit..." key
