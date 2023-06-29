e28090ea4388366a701e88c2d01cf84c31916935
Mylo <36931363+gitmylo@users.noreply.github.com>
2023-06-29 18:52:45 +0200

    New styling system

---
+++ simplestyle/manager.py
@@ -0,0 +1,55 @@
+class RawStyleValue:
+    def __init__(self, value):
+        stack[-1].stack[-1].values.append(value)
+
+
+class StyleValue(RawStyleValue):
+    def __init__(self, key, value):
+        super().__init__(f'{key}: {value};')
+
+
+class StyleRule:
+    def __init__(self, selector):
+        self.selector = selector
+        self.values = []
+
+    def __enter__(self):
+        stack[-1].stack.append(self)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        stack[-1].rules.append(stack[-1].stack.pop())
+
+
+
+class SimpleStyle:
+    def __init__(self, priority=0):
+        self.priority = priority
+        self.stack = []
+        self.rules = []
+
+    def __enter__(self):
+        stack.append(self)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        rules.append(stack.pop())
+
+
+stack: list[SimpleStyle] = []
+
+rules: list[SimpleStyle] = []
+
+
+def create_stylesheet():
+
+    def sort_key(value):
+        return value.priority
+
+    full_sheet = ''
+    rules.sort(key=sort_key)
+    for rule in rules:
+        for stylerule in rule.rules:
+            full_sheet += f'{stylerule.selector} {{'
+            for stylevalue in stylerule.values:
+                full_sheet += stylevalue
+            full_sheet += '}'
+    return full_sheet
--- webui/ui/ui.py
+++ webui/ui/ui.py
@@ -6,65 +6,56 @@ tabs_el: gr.Tabs = None
 
 def create_ui(theme) -> gr.Blocks:
 
-    css = """
-    .gradio-container {
-        max-width: calc(100% - 100px) !important;
-    }
-    
-    .tabitem:not(.tabitem .tabitem) {
-        height: calc(100vh - 100px) !important;
-        overflow: auto;
-    }
-    
-    .tool{
-        max-width: 2.2em;
-        min-width: 2.2em !important;
-        height: 2.4em;
-        align-self: end;
-        line-height: 1em;
-        border-radius: 0.5em;
-    }
-    
-    .smallsplit {
-        max-width: 2.2em;
-        min-width: 2.2em !important;
-        align-self: end;
-        border-radius: 0.5em;
-    }
-    
-    .offset--10 {
-        transform: translateY(-10px);
-    }
-    
-    .text-center {
-        text-align: center;
-    }
-    
-    .padding-h-0 {
-        padding-left: 0 !important;
-        padding-right: 0 !important;
-    }
-    
-    table:not(.file-preview) {
-        border: 1px solid white !important;
-        margin-left: auto;
-        margin-right: auto;
-    }
-    
-    table a {
-        color: white !important;
-    }
-    
-    table th, table td {
-        padding: 10px !important;
-    }
-    
-    .center-h {
-        margin-left: auto !important;
-        margin-right: auto !important;
-        text-align: center !important;
-    }
-    """
+    from simplestyle.manager import create_stylesheet, SimpleStyle, StyleRule, StyleValue
+
+    with SimpleStyle(priority=0):
+        with StyleRule('.gradio-container'):
+            StyleValue('max-width', 'calc(100% - 100px) !important')
+
+        with StyleRule('.tabitem:not(.tabitem .tabitem)'):
+            StyleValue('height', 'calc(100vh - 100px) !important')
+            StyleValue('overflow', 'auto')
+
+        with StyleRule('.tool'):
+            StyleValue('max-width', '2.2em')
+            StyleValue('min-width', '2.2em !important')
+            StyleValue('height', '2.4em')
+            StyleValue('align-self', 'end')
+            StyleValue('line-height', '1em')
+            StyleValue('border-radius', '0.5em')
+
+        with StyleRule('.smallsplit'):
+            StyleValue('max-width', '2.2em')
+            StyleValue('min-width', '2.2em !important')
+            StyleValue('align-self', 'end')
+            StyleValue('border-radius', '0.5em')
+
+        with StyleRule('.offset--10'):
+            StyleValue('transform', 'translateY(-10px)')
+
+        with StyleRule('.text-center'):
+            StyleValue('text-align', 'center')
+
+        with StyleRule('.padding-h-0'):
+            StyleValue('padding-left', '0 !important')
+            StyleValue('padding-right', '0 !important')
+
+        with StyleRule('table:not(.file-preview)'):
+            StyleValue('border', '1px solid white !important')
+            StyleValue('margin-left', 'auto')
+            StyleValue('margin-right', 'auto')
+
+        with StyleRule('table a'):
+            StyleValue('color', 'white !important')
+
+        with StyleRule('table th, table td'):
+            StyleValue('padding', '10px !important')
+
+        with StyleRule('.center-h'):
+            StyleValue('margin-left', 'auto !important')
+            StyleValue('margin-right', 'auto !important')
+            StyleValue('text-align', 'center !important')
+
 
     tabs = [
         ('📜▶🗣 Text to speech', text_to_speech),
@@ -78,7 +69,7 @@ def create_ui(theme) -> gr.Blocks:
         ('🧾 Info', info_tab)
     ]
     global tabs_el
-    with gr.Blocks(theme=theme, title='🔊Audio WebUI🎵', css=css) as webui:
+    with gr.Blocks(theme=theme, title='🔊Audio WebUI🎵', css=create_stylesheet()) as webui:
         with gr.Tabs() as tabs_element:
             tabs_el = tabs_element
             for name, content in tabs:
