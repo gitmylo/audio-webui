f3034336fec1fcf0c42f72e380f9e08c9edba820
Mylo <36931363+gitmylo@users.noreply.github.com>
2023-07-02 22:24:04 +0200

    Update installers

--- installers/install_windows.bat
+++ installers/install_windows.bat
@@ -1,8 +1,2 @@
 @echo off
-mkdir "audio-webui"
-cd "audio-webui"
-git init
-git remote add origin https://github.com/gitmylo/audio-webui
-git fetch
-git reset origin/master  # Required when the versioned files existed in path before "git init" of this repo.
-git checkout -t origin/master -f
+git clone https://github.com/gitmylo/audio-webui
