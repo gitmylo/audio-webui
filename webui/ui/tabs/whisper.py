12a9cb21bd66936d1a0f67e46c0069841c343ede
mylo <36931363+gitmylo@users.noreply.github.com>
2023-07-15 21:01:02 +0200

    Whisper ids on some elements

--- webui/ui/tabs/whisper.py
+++ webui/ui/tabs/whisper.py
@@ -13,14 +13,14 @@ def whisper():
             def load_model(model):
                 return w.load(model)
         with gradio.Row():
-            transcribe = gradio.Button('Transcribe', variant='primary')
+            transcribe = gradio.Button('Transcribe', variant='primary', elem_id='whisper-transcribe')
     with gradio.Row():
         with gradio.Column():
-            audio = gradio.Audio(label='Audio to transcribe')
-            audios = gradio.Files(label='Batch input', file_types=['audio'])
+            audio = gradio.Audio(label='Audio to transcribe', elem_id='whisper-audio-in')
+            audios = gradio.Files(label='Batch input', file_types=['audio'], elem_id='whisper-batch-in')
         with gradio.Column():
-            output = gradio.TextArea(label='Transcript')
-            outputs = gradio.Files(label='Batch output', file_types=['audio'])
+            output = gradio.TextArea(label='Transcript', elem_id='whisper-audio-out')
+            outputs = gradio.Files(label='Batch output', file_types=['audio'], elem_id='whisper-batch-out')
 
     unload.click(fn=w.unload, outputs=output, show_progress=True)
     load.click(fn=load_model, inputs=selected, outputs=output, show_progress=True)
