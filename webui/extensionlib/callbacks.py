3256dd221f07ceeb5bf14765bc6bd2ff01d233e3
mylo <36931363+gitmylo@users.noreply.github.com>
2023-07-18 21:37:30 +0200

    Some boilerplate or whatever it's called

--- webui/extensionlib/callbacks.py
+++ webui/extensionlib/callbacks.py
@@ -6,11 +6,22 @@ class CallBack:
     def call(self, *args, **kwargs):
         self.callback(*args, **kwargs)
 
+    def __call__(self, *args, **kwargs):
+        self.call(*args, **kwargs)
+
 
 class CallBackManager:
     def __init__(self, name):
         self.name = name
         self.callbacks: list[CallBack] = []
+
+    def register(self, callback: CallBack):
+        self.callbacks.append(callback)
+        self.callbacks.sort(key=lambda c: c.priority, reverse=True)
+
+    def call(self, *args, **kwargs):
+        for cb in self.callbacks:
+            cb(*args, **kwargs)
 
 
 callbacks: list[CallBackManager] = []
---
+++ webui/extensionlib/extensionstates.py
@@ -0,0 +1,9 @@
+from webui.extensionlib.extensionInfo import ExtensionInfo
+
+
+class ExtensionState:
+    def __init__(self, extension_dir_name):
+        pass  # Load extension
+
+
+states: dict[str, ExtensionState] = {}
