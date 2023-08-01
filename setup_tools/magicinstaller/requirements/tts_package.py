b5f89150f7f453aa5a2a8be4268126f8e834ed3e
mylo <36931363+gitmylo@users.noreply.github.com>
2023-08-01 16:34:14 +0200

    Include japanese "optional" package, because it's not actually optional and causes crashes for some users. should fix #91

--- setup_tools/magicinstaller/requirements/tts_package.py
+++ setup_tools/magicinstaller/requirements/tts_package.py
@@ -3,3 +3,6 @@ from setup_tools.magicinstaller.requirement import SimpleRequirement
 
 class TTS(SimpleRequirement):
     package_name = 'TTS'
+
+    def install(self) -> tuple[int, str, str]:
+        return self.install_pip("tts[ja]", "TTS")
